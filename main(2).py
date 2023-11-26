469257512b6a9dc705cf0926b13f9372ef0fb4db
BarkovMaxim00 <148942592+BarkovMaxim00@users.noreply.github.com>
2023-11-26 18:25:21 +0300

    Лабораторная работа 4

---
+++ main(2).py
@@ -0,0 +1,26 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open("input.csv", 'r') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter = ',',  quotechar = '\n')
+        reader_list = [content for content in reader]
+
+    # TODO считать содержимое csv файла
+    with open("output.json", 'w') as json_file:
+            json.dump(reader_list, json_file, indent = 4)
+
+    ...  # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
